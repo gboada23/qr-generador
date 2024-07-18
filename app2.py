c46d37a73e61c3c71cb7427196f629a2f949f6f3
Gustavo Boada <110425968+gboada23@users.noreply.github.com>
2024-07-18 10:06:15 -0400

    Update app2.py
    añadi manejo de errores

--- app2.py
+++ app2.py
@@ -4,44 +4,50 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Generador QR | Serviplus",
-    page_icon="⭐️")
-col1, col2, col3 = st.columns((1,5,1))
-col2.title("Generador de Codigos Qr")
+    page_icon="⭐️"
+)
+
+col1, col2, col3 = st.columns((1, 5, 1))
+col2.title("Generador de Códigos QR")
+
 def generate_qr_code():
+    nombre = col2.text_input(label="Ingrese el código cifrado de la agencia:", value="", max_chars=None, key="nombre")
+    NOMBRE = col2.text_input(label="Ingrese el código de la agencia relacionada al código cifrado:", value="", max_chars=None, key="NOMBRE")
     
-    col2.write("")
-    nombre = col2.text_input(label="Ingrese el codigo cifrado de la agencia:", value="", max_chars=None, key=None)
-    NOMBRE = col2.text_input(label="Ingrese el codigo de la agencia relacionada al codigo cifrado:", value="", max_chars=None, key=None)
-    if nombre == "":
+    if not nombre:
         col2.write("Por favor introduce el nombre del Personal")
     else:
-        Logo_link = 'Logo SP.png'
-        logo = Image.open(Logo_link)
-    
-        basewidth = 65
-        wpercent = (basewidth/float(logo.size[0]))
-        hsize = int((float(logo.size[1])*float(wpercent)))
-        logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
-        QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-    
-        QRcode.add_data(nombre)
-        QRcode.make()
+        try:
+            Logo_link = 'Logo SP.png'
+            logo = Image.open(Logo_link)
         
-        QRcolor = 'black'
-        QRimg = QRcode.make_image(fill_color=QRcolor, back_color="white").convert('RGB')
-    
-        pos = ((QRimg.size[0] - logo.size[0]) // 2, (QRimg.size[1] - logo.size[1]) // 2)
-        QRimg.paste(logo, pos)
+            basewidth = 65
+            wpercent = (basewidth/float(logo.size[0]))
+            hsize = int((float(logo.size[1])*float(wpercent)))
+            logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+            QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
         
-        col2.image(QRimg, width=300)
-        col2.write(f"QR generado para {nombre}")
-        img = f'{NOMBRE}.png'
-        QRimg.save(img)
-        if col2.download_button("Descargar QR", open(img, 'rb').read(), file_name=f'{NOMBRE}.png', mime='image/png'):
-            col2.success("QR generado y descargado!")
+            QRcode.add_data(nombre)
+            QRcode.make()
             
+            QRcolor = 'black'
+            QRimg = QRcode.make_image(fill_color=QRcolor, back_color="white").convert('RGB')
+        
+            pos = ((QRimg.size[0] - logo.size[0]) // 2, (QRimg.size[1] - logo.size[1]) // 2)
+            QRimg.paste(logo, pos)
+            
+            col2.image(QRimg, width=300)
+            col2.write(f"QR generado para {nombre}")
+            img = f'{NOMBRE}.png'
+            QRimg.save(img)
+            if col2.download_button("Descargar QR", open(img, 'rb').read(), file_name=f'{NOMBRE}.png', mime='image/png'):
+                col2.success("QR generado y descargado!")
+        except Exception as e:
+            col2.error(f"Error al generar el QR: {e}")
+
 generate_qr_code()
 st.write("#")
 st.write("#")
 st.caption("APP creada por Gustavo Boada Coord de Datos.")
-st.caption("Contactame [Linkdein](https://www.linkedin.com/in/gboada23/)")
+st.caption("Contactame [LinkedIn](https://www.linkedin.com/in/gboada23/)")
+
